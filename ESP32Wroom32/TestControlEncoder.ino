45a27e85858a5e96248ff9cda66e3cef44154463
KongphopTongdee <119693093+KongphopTongdee@users.noreply.github.com>
2025-12-28 22:27:04 +0700

    Update TestControlEncoder.ino
    Error:
    1.Can't run more than 1 iteration
    2.Can't declare to global variable

--- ESP32Wroom32/TestControlEncoder.ino
+++ ESP32Wroom32/TestControlEncoder.ino
@@ -1,18 +1,21 @@
 #include <ESP32Encoder.h>
+#include <Arduino.h>
 
 // Pin usage => (32 = encoderD3), (33 = encoderD2), (25 = PWM), (26 = Dir)
 #define PWMpin          25
 #define DIRpin          26
-#define encoderD3       34
-#define encoderD2       35
+#define encoderD3       32
+#define encoderD2       33
 
 // Declare class for encoder
 ESP32Encoder encoder;
 
 // Declare the setup motor drive board
-const int pwmChannel = 0;
-const int pwmFreqency = 20000;          // Quite motor with more frequency in one duty cycle
+const int pwmFrequency = 20000;          // Quite motor with more frequency in one duty cycle
 const int pwmResolution = 8;
+
+// unsigned long currentMillis = millis();
+// unsigned long previousMillis = 0;
 
 // Create function limit value
 int limitValue( int inputValue, int minValue, int maxValue ){
@@ -31,7 +34,7 @@ int limitValue( int inputValue, int minValue, int maxValue ){
 
 // Function for control speed of motor
 void setMotorSpeed( int speedMotor ){
-    limitSpeed = limitValue( speedMotor, -255, 255 );
+    int limitSpeed = limitValue( speedMotor, -255, 255 );
     
     // if value is posible => motor forward
     if( limitSpeed > 0 ){
@@ -54,36 +57,37 @@ void setMotorSpeed( int speedMotor ){
         ledcWrite( PWMpin, limitSpeed );
     }
 }
-void displayOutput(){
-    // Print serial count
-    Serial.println( "Encoder count: " + String( encoder.getCount() ) );
+
+void displayCount(){
+	// Print serial count
+	Serial.println( "Encoder count: " + String( encoder.getCount() ) );
 }
 
-// Create counter function similar to delay function which using the pointer
-void similarDelaySetMotor( int valueTimer, void (*func)(int), int inputValueFunc ){
-    // Time fucntion
+// Create counter function similar to delay function which visualize the count of encoder
+void displayAndDelay( int valueTimer ){
+    //Time function
     unsigned long currentMillis = millis();
-    unsigned long previousMillis = 0;
-    if( currentMillis - previousMillis >= valueTimer ){
-        previousMillis = currentMillis;
-        
-        // Call function which using pointer
-        func( inputValueFunc );
-    }
-    
+	unsigned long previousMillis = 0;
+	if( currentMillis - previousMillis >= valueTimer ){
+		previousMillis = currentMillis;
+		
+		// Visualize the encoder count
+		displayCount();
+	}
+
 }
 
-// Create counter function similar to delay function which using the pointer
-void similarDelay( int valueTimer, void (*func)(int) ){
-    // Time fucntion
-    unsigned long currentMillis = millis();
-    unsigned long previousMillis = 0;
-    if( currentMillis - previousMillis >= valueTimer ){
-        previousMillis = currentMillis;
-        
-        // Call function which using pointer
-        func();
-    }
+// Create counter function similar to delay function which using the pointer to another function
+void similarDelay( int valueTimer, void (*func)(int), int inputValueFunc ){
+	// Time fucntion
+	unsigned long currentMillis = millis();
+	unsigned long previousMillis = 0;
+	if( currentMillis - previousMillis >= valueTimer ){
+		previousMillis = currentMillis;
+		
+		// Call function which using pointer
+		func( inputValueFunc );
+	}
     
 }
 
@@ -91,7 +95,7 @@ void setup() {
   Serial.begin(115200);
 
   // Setup motor encoder with pullup
-  ESP32Encoder::useInternalWeakPullResistors = UP;
+  ESP32Encoder::useInternalWeakPullResistors = puType::up;
   encoder.attachFullQuad( encoderD3, encoderD2 );
   encoder.clearCount();
   // set count value
@@ -99,15 +103,22 @@ void setup() {
 
   // Setup Motor( ledcLibrary )
   pinMode(DIRpin, OUTPUT);
-  ledcSetup(pwmChannel, pwmFreqency, pwmResolution);
-  ledcAttachPin(PWMpin, pwmChannel);
+  //   After esp32 core >= v.3 use this code:
+  ledcAttach(PWMpin, pwmFrequency, pwmResolution);
 }
 
-void loop() {
-    similarDelay( 100, displayOutput );
-    
-    // Call function timer and set speed motor
-    similarDelaySetMotor( 2000, setMotorSpeed, 100 );
-    similarDelaySetMotor( 2000, setMotorSpeed, 0 );
-    similarDelaySetMotor( 2000, setMotorSpeed, -100 );
+void loop() {	
+	// Call function timer and set speed motor
+    displayAndDelay( 2000 );
+    // for( int iteration = 0; iteration <= 5; iteration++ ){
+    //     similarDelay( 5000, setMotorSpeed, 100 );
+    //     similarDelay( 10000, setMotorSpeed, 0 );
+    //     similarDelay( 15000, setMotorSpeed, -100 );
+    //     similarDelay( 20000, setMotorSpeed, 0 );
+    //     // previousMillis = currentMillis;   
+    // }
+    similarDelay( 5000, setMotorSpeed, 100 );
+    similarDelay( 10000, setMotorSpeed, 0 );
+    similarDelay( 15000, setMotorSpeed, -100 );
+    similarDelay( 20000, setMotorSpeed, 0 );
 }
